3314dcb69f809bbb79ce49d546e4d687c4fece8e
Don Fox <dfox09@gmail.com>
2024-04-01 15:14:37 -0400

    Remove formatter and settings plugin

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { plugin_completer } from './completer/index';
 import { plugin_editor } from './editor/index';
-import { plugin_formatting } from './formatter/index';
 import { plugin_widget } from './widgets/index';
-import { plugin_settings } from './settings/index';
 
 export * from './version';
 export default [
   plugin_completer,
   plugin_editor,
-  plugin_formatting,
-  plugin_widget,
-  plugin_settings
+  plugin_widget
 ];
