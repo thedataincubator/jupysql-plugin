47fae7c10613750ca06edc40cb794e0f04aa6ddf
Don Fox <don@thedataincubator.com>
2024-04-30 13:19:15 -0400

    Comment out editor plugin
    The editor plugin is responsible for rendering a cell with the SQL
    magic as SQL code. However, it defaults to Python if it's not an SQL
    magic cell which is problematic since not all cells are Python. Some
    are Bash or markdown. Note, the markdown is only rendered as Python
    when you are trying to edit the cell. It's displayed fine when leaving
    the editing mode.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { plugin_completer } from './completer/index';
-import { plugin_editor } from './editor/index';
+// import { plugin_editor } from './editor/index';
 import { plugin_widget } from './widgets/index';
 
 export * from './version';
 export default [
   plugin_completer,
-  plugin_editor,
+  // plugin_editor,
   plugin_widget
 ];
